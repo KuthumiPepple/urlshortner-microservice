e9b0de176c39518c52d5fbdf037d38e2297de85e
Kuthumi Pepple <kuthumipepple@gmail.com>
2021-07-19 09:02:19 +0100

    use express's urlencoded instead of body-parser

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.urlencoded({extended: false}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
